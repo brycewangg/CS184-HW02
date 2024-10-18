bf2a6630fb2b5c629b14ce0a1cd6436dfc350e0c
Bryce <brycewang@ucsb.edu>
2024-10-17 20:11:57 -0700

    Initial Commit

---
+++ Authentication/AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationApp.swift
+//  Authentication
+//
+//  Created by Bryce Wang on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthenticationTests/AuthenticationTests.swift
@@ -0,0 +1,16 @@
+//
+//  AuthenticationTests.swift
+//  AuthenticationTests
+//
+//  Created by Bryce Wang on 10/17/24.
+//
+
+import Testing
+
+struct AuthenticationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
